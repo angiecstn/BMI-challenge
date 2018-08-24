0e8aeaef7886f2e166672c306a84c9b6a9de9b70
Angelica Costantini <angelicacostantini07@gmail.com>
2018-08-24 10:22:20 +0200

    add test for imperial method and formula

--- src/js/person.js
+++ src/js/person.js
@@ -7,3 +7,9 @@ Person.prototype.calculate_bmi = function () {
     calculator = new BMICalculator();
     calculator.metric_bmi(this)
 };
+
+
+Person.prototype.calculate_bmi_imperial= function () {
+    calculator = new BMICalculator();
+    calculator.imperial_bmi(this)
+};
